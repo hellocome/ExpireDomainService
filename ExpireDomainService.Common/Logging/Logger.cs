b637d5ef7d80574bd61fe0295ba4ebd3f3509ffb
Haijie Zhao <zhaohaijie@gmail.com>
2017-03-20 16:43:37 +0000

    check in

---
+++ ExpireDomainService.Common/Logging/Logger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpireDomainService.Common.Logging
+{
+    public sealed class Logger
+    {
+        private static ILogger instance = LoggerFactory.getLogger();
+
+        public static ILogger Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+    }
+}
--- ExpireDomainService.Core/Scheduler/SchedulerThread.cs
+++ ExpireDomainService.Core/Scheduler/SchedulerThread.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading;
+using ExpireDomainService.Common.Logging;
 
 namespace ExpireDomainService.Core.Schedule
 {
