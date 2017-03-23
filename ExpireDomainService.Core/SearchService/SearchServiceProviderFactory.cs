de94bae125ad69ece96660df4ee98ed7cfd25c43
Haijie Zhao <zhaohaijie@gmail.com>
2017-03-23 08:39:24 +0000

    Search service provider

---
+++ ExpireDomainService.Core/SearchService/SearchServiceProviderFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpireDomainService.Core.SearchService
+{
+    public class SearchServiceProviderFactory
+    {
+        public static IDomainNameSearchServiceProvider GetSearchServiceProvider()
+        {
+            return null;
+        }
+    }
+}
---
+++ ExpireDomainService.SearchServiceProvider/SimpleSearchServiceProvider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using ExpireDomainService.Core.Domains;
+using ExpireDomainService.Common.Collection;
+using ExpireDomainService.Core.SearchService;
+
+namespace ExpireDomainService.SearchServiceProvider
+{
+    public sealed class SimpleSearchServiceProvider : IDomainNameSearchServiceProvider
+    {
+        public void UpdateSearchData(Dictionary<string, PagedSet<ExpireDomainName>> searchData)
+        {
+        }
+
+        public IDomainNameSearchService DomainNameSearchService
+        {
+            get
+            {
+                return null;
+            }
+        }
+    }
+}
